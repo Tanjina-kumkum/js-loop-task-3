11357c4a1756d149ec1e59154a875e732592019f
Tanjina-kumkum <tanjinaakter882@gmail.com>
2025-08-02 00:34:36 +0600

    while loop with even and odd number added

---
+++ while.even.odd.js
@@ -0,0 +1,16 @@
+let num = 61;
+while(num<=100){
+    if(num%2===1){
+        console.log(num);
+        
+    }
+    num++
+}
+let num1 = 78;
+while(num1<=98){
+    if(num1%2===0){
+        console.log(num1);
+        
+    }
+    num1++
+}
---
+++ while.js
@@ -0,0 +1,10 @@
+let number = 6;
+while(number<=60){
+console.log(number);
+number++
+}
+let number1 = 1;
+while(number1<=60){
+console.log(number1);
+number1++
+}
